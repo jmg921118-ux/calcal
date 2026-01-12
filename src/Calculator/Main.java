092199d7d04b802b549c831c808fcae206b4b4d3
mingyo <jmg921118@gmail.com>
2026-01-12 23:24:18 +0900

    계산기 더하기까지 구현 완료/ 곱하기 빼기를 해도 더하기로 출력 ㅠㅠ

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,7 +1,33 @@
 package Calculator;
 
-public class Main {
-    static void main() {
+import java.util.Scanner;
 
-    }
-}
+public class Main {
+   public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력해주세요");
+        int number1 = sc.nextInt();
+        System.out.println("기호를 입력해주세요");
+        char operator = sc.next().charAt(0);
+       System.out.println("숫자를 입력해주세요");
+       int number2 = sc.nextInt();
+       int sum = (number1 + number2);
+
+        if (operator == '+') {
+
+        } else if (operator == '*') {
+
+        }
+            else if (operator == '-') {
+
+        }
+
+        }
+   }
+
+
+
+
+
+
