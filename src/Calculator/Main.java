73a24a8f54b83ef7fb578e7037484aff01d5edca
mingyo <jmg921118@gmail.com>
2026-01-13 21:42:05 +0900

    계산기ㅣ step1 모든 기능 구현 완료!!

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -3,60 +3,49 @@ package Calculator;
 import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        System.out.println("안녕하세요 계산기입니다.");
         Scanner sc = new Scanner(System.in);
-        System.out.println("숫자를 입력해주세요");
-        int number1 = sc.nextInt();
-        System.out.println("기호를 입력해주세요");
-        char operator = sc.next().charAt(0);
-       System.out.println("숫자를 입력해주세요");
-       int number2 = sc.nextInt();
+        String command = "";
 
+        do {
+            try {
+                System.out.println("숫자를 입력해주세요");
+                int number1 = sc.nextInt();
 
+                System.out.println("기호를 입력해주세요");
+                char operator = sc.next().charAt(0);
 
+                System.out.println("숫자를 입력해주세요");
+                int number2 = sc.nextInt();
 
-
-
-
-
-        if (operator == '+') {
-            int sum = (number1 + number2); // 더하기 방에 들어왔을 때만 더하기!
-            System.out.println("결과값입니다. = " + sum);
-
-        } else if (operator == '*') {
-            int multi = (number1 * number2);
-            System.out.println("결과값입니다. = " + multi);
-        }
-            else if (operator == '-') {
-            int minus = (number1 - number2);
-            System.out.println("결과값입니다. = " + minus);
-        }
-            else if (operator == '/') {
-
-                try{
-                    // [시도] 위험한 계산은 반드시 이 중괄호 안에서 해야 합니다!
-                    // (주의: 계산식을 여기 밖에서 미리 써두면, 들어오기 전에 이미 프로그램이 터집니다.)
-                    int result = number1 / number2;
-                    System.out.println("결과값입니다. = " + result);
-                }catch (ArithmeticException e) {
-                    // [수습] 0으로 나눠서 에러가 나면, 프로그램이 죽지 않고 바로 여기로 옵니다.
-                    System.out.println("0으로 나눌 수 없습니다!.");
-
+                if (operator == '+') {
+                    System.out.println("결과값입니다. = " + (number1 + number2));
+                } else if (operator == '*') {
+                    System.out.println("결과값입니다. = " + (number1 * number2));
+                } else if (operator == '-') {
+                    System.out.println("결과값입니다. = " + (number1 - number2));
+                } else if (operator == '/') {
+                    try {
+                        int result = number1 / number2;
+                        System.out.println("결과값입니다. = " + result);
+                    } catch (ArithmeticException e) {
+                        System.out.println("0으로 나눌 수 없습니다!.");
+                    }
+                } else {
+                    System.out.println("지원하지 않는 연산자입니다.");
                 }
+            } catch (Exception e) {
+                System.out.println("입력 오류가 발생했습니다. 숫자를 정확히 입력해주세요.");
+                sc.nextLine(); // 버퍼 비우기
+            }
 
+            System.out.println("계속하시겠습니까? (종료하려면 'exit' 입력, 계속하려면 아무 키나 입력)");
+            command = sc.next();
 
+        } while (!command.equalsIgnoreCase("exit")); // 대소문자 구분 없이 exit 처리
 
-
-        }
-
-
-
-        }
-   }
-
-
-
-
-
-
+        System.out.println("계산기를 종료합니다.");
+        sc.close();
+    } // main 메서드 끝
+} // Main 클래스 끝
